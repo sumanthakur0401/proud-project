e6f26609abbdb8608bebb62e2a27f9ce9124b9cf
Suman Thakur <sumanthakur4011999@gmail.com>
2024-01-08 23:00:12 +0530

    Adding profileSection

--- pages/Volunteer/info.js
+++ pages/Volunteer/info.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '@/pages/Volunteer/components/navBar'
+import ProfileBar from '@/pages/Volunteer/components/profileBar'
 
 const info = () => {
   return (
     <>
         <NavBar/>
+        <ProfileBar/>
     </>
   )
 }
--- pages/Volunteer/wonderland.js
+++ pages/Volunteer/wonderland.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '@/pages/Volunteer/components/navBar'
+import ProfileBar from '@/pages/Volunteer/components/profileBar'
 
 const wonderland = () => {
   return (
     <>
       <NavBar/>
+      <ProfileBar/>
     </>
   )
 }
