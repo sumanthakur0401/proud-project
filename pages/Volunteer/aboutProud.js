d9f023009f6e4d644f3ede6eedaf03b5c56d5160
Suman Thakur <sumanthakur4011999@gmail.com>
2024-01-17 14:23:07 +0530

    Update - aboutProud Page

--- pages/Volunteer/aboutProud.js
+++ pages/Volunteer/aboutProud.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '@/styles/Vol_styles/aboutProud.module.css'
 import TempNavBar from '@/pages/Volunteer/components/tempNavBar'
 import BackToTopButton from './components/BackToTopButton'
 
 const aboutProud = () => {
+
+    const [isRed, setIsRed] = useState(false);
+
+    const changeColor = () => {
+        setIsRed((prev) => !prev);
+    };
+
   return (
   <>
-  <TempNavBar/>
-  <BackToTopButton/>
+    <TempNavBar/>
+    <BackToTopButton/>
     <div className={styles.screen}>
     
         <div className={styles.aboutProudSection}>
@@ -15,7 +22,7 @@ const aboutProud = () => {
             <h1 className={styles.describProudText}>Where NGOs meet eager volunteers, catalyzing change together.</h1>
             <div className={styles.buttonsSection}>
                 <button className={styles.exploreButton}>
-                    Explore now!
+                        Explore now!
                 </button>
 
                 <button className={styles.contactButton}>
@@ -49,20 +56,74 @@ const aboutProud = () => {
             <div className={styles.thirdSectionDataContainer}>
                 <div className={styles.thirdSectionAboutBoxData}>
                     <p className={styles.thirdSectionBoxText}>
-                        "Discover a variety of NGOs in one place with a single click for effortless volunteering opportunities.""
+                        "Discover a variety of NGOs in one place for effortless volunteering opportunities."
                     </p>
                     <p className={styles.thirdSectionBoxSubText}>
-                        Easily find and join volunteer activities with just one click, making it simple and efficient for you to get involved and make a positive impact
+                    Effortlessly explore a range of non-profit organizations all in one place. This centralized platform makes it easy for you to discover various volunteering opportunities, ensuring a seamless and meaningful engagement with causes that matter to you
                     </p>
                 </div>
 
                 <div className={styles.thirdSectionDemoBox}>
-                    <p className={styles.thirdSectionDemoText}>
-                        click here to join us!
+                    <div className={styles.smallBoxes}></div>
+                    <div className={styles.smallBoxes}></div>
+                    <div className={styles.smallBoxes}></div>
+                    <div className={styles.smallBoxes}></div>
+                    <div className={styles.smallBoxes}></div>
+                    <div className={styles.smallBoxes}></div>
+                    <p className={styles.smallBoxesText}>Ngo's Wonderland</p>
+                </div>
+            </div>
+        </div>
+
+        <div className={styles.fourthProudSection}>
+            <div className={styles.fourthSectionDataContainer}>
+
+                <div className={styles.fourthSectionDemoBox}>
+                    <p className={styles.scrollText}>scroll here</p>
+                    <div className={styles.demoScroll}>
+                        <div className={styles.smallContainer}>
+                            <p className={styles.smallBoxText}>
+                                Click heart if you like it
+                            </p>
+                            <div className={`${styles.heart} ${isRed ? styles.red : ''}`} onClick={changeColor}>
+                                &#10084;
+                            </div>
+                        </div>
+                        <div className={styles.smallContainer}>
+                            <p className={styles.smallBoxText}>
+                                Click heart if you like it
+                            </p>
+                            <div className={`${styles.heart} ${isRed ? styles.red : ''}`} onClick={changeColor}>
+                                &#10084;
+                            </div>
+                        </div>
+                        <div className={styles.smallContainer}>
+                            <p className={styles.smallBoxText}>
+                                Click heart if you like it
+                            </p>
+                            <div className={`${styles.heart} ${isRed ? styles.red : ''}`} onClick={changeColor}>
+                                &#10084;
+                            </div>
+                        </div>
+                        <div className={styles.smallContainer}>
+                            <p className={styles.smallBoxText}>
+                                Click heart if you like it
+                            </p>
+                            <div className={`${styles.heart} ${isRed ? styles.red : ''}`} onClick={changeColor}>
+                                &#10084;
+                            </div>
+                        </div>
+                    </div>
+                    <p className={styles.scrollBoxesText}><img className={styles.proudLogoWithText} src='/assets/lightBlueProudLogo.png'/> Moments</p>
+                </div>
+
+                <div className={styles.fourthSectionAboutBoxData}>
+                    <p className={styles.fourthSectionBoxText}>
+                        "Explore 'Moments' - a place where good vibes and great deeds meet!"
                     </p>
-                    <button className={styles.thirdSectionDemoBoxButton}>
-                        Join now!
-                    </button>
+                    <p className={styles.fourthSectionBoxSubText}>
+                        Visit 'Moments' to showcase your achievements! Share photos and videos of your volunteering, and see NGOs highlight impactful activities
+                    </p>
                 </div>
             </div>
         </div>
--- pages/Volunteer/components/tempNavBar.js
+++ pages/Volunteer/components/tempNavBar.js
@@ -7,7 +7,7 @@ const tempNavBar = () => {
     <div className={styles.tempNavDiv}>
         <div className={styles.navBar}>
             <img className={styles.logoImg} src='/assets/blueProudLogo.png'/>
-            <img className={styles.githubIcon} src='/assets/githubIcon.png'/>
+            <a href='https://github.com/sumanthakur0401/proud-project'><img className={styles.githubIcon} src='/assets/githubIcon.png'/></a>
         </div>
     </div>
   )
