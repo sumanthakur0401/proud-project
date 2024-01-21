51e4fcfa13e6ec3eb0609cbd0529d8392e139bae
Suman Thakur <sumanthakur4011999@gmail.com>
2024-01-21 19:26:39 +0530

    Update-home page & about page/Adding Dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/index.module.css'
 const inter = Inter({ subsets: ['latin'] })
