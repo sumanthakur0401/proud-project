e5567752938efc1bb43535d9a965dbc161a9d3cb
Suman Thakur <sumanthakur4011999@gmail.com>
2024-01-21 19:20:37 +0530

    Update-home page & about page/Adding Dark mode

--- pages/Volunteer/expandedGreenCard.js
+++ pages/Volunteer/expandedGreenCard.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import styles from '@/styles/Vol_styles/expandedGreenCard.module.css'
-import NavBar from '@/pages/Volunteer/components/navBar'
-import ProfileBar from '@/pages/Volunteer/components/profileBar'
 import Link from 'next/link'
-
 
 const expandedGreenCard = () => {
   return (
     <>
-      <NavBar />
+     
       <div className={styles.midSection}>
         <div className={styles.ngoSection}>
           <p className={styles.ngoNameText}>Saarthi</p>
@@ -16,10 +13,8 @@ const expandedGreenCard = () => {
         </div>
         <Link href='home'><img className={styles.backButtonIcon} src='/assets/backButtonIcon.png'/></Link>
         <div className={styles.ngoUpdateCard}>
-
         </div>
       </div>  
-      <ProfileBar/>
     </>
   )
 }
