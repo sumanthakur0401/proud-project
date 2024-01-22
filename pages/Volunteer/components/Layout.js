1ff7d4af18f0804427c38094a32ab59a6312075a
Suman Thakur <sumanthakur4011999@gmail.com>
2024-01-22 20:05:54 +0530

    Updates

--- pages/Volunteer/components/Layout.js
+++ pages/Volunteer/components/Layout.js
@@ -1,48 +1,52 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './navBar';
 import ProfileBar from './profileBar';
-import styles from '../../../styles/Vol_styles/layout.module.css'
-
+import styles from '../../../styles/Vol_styles/layout.module.css';
 
 const Layout = ({ children }) => {
+  const [isChecked, setIsChecked] = useState(false);
 
-    const [isChecked, setIsChecked] = useState(false);
+  useEffect(() => {
+    const storedDarkMode = localStorage.getItem('darkMode');
+    if (storedDarkMode) {
+      setIsChecked(storedDarkMode === 'true');
+    }
+  }, []);
 
-    // Load the initial state from local storage on component mount
-    useEffect(() => {
-      const storedDarkMode = localStorage.getItem('darkMode');
-      if (storedDarkMode) {
-        setIsChecked(storedDarkMode === 'true');
-      }
-    }, []);
-  
-    const handleToggle = () => {
-      // Toggle the state
-      const newChecked = !isChecked;
-      setIsChecked(newChecked);
-      
-      // Store the new state in local storage
-      localStorage.setItem('darkMode', newChecked.toString());
-    };
-
-    return (
-      <div className={isChecked ? styles.backGroundBlack : styles.backGround}>
-        <NavBar />
-        <ProfileBar/>
-        <div className="content">{children}</div>
-        
-        <div className={styles.switchModeButton}>
-          <label className={styles.switch}>
-            <input type="checkbox" onChange={handleToggle} checked={isChecked}/>
-            <span className={styles.slider}></span>
-          </label>
-       </div>
-       <div className={styles.switchText}>
-          Switch 
-          Mode!
-       </div>
-      </div>
-    );
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+    localStorage.setItem('darkMode', isChecked ? 'false' : 'true');
   };
-  
-  export default Layout;
+
+  return (
+    <div className={styles.wrapper}>
+      {isChecked && (
+        <div className={styles.backGroundBlack}>
+          <NavBar />
+          <ProfileBar />
+          <div className="content">{children}</div>
+        </div>
+      )}
+      {!isChecked && (
+        <div className={styles.backGround}>
+          <NavBar />
+          <ProfileBar />
+          <div className="content">{children}</div>
+        </div>
+      )}
+      <div className={styles.switchModeButton}>
+        <label className={styles.switch}>
+          <input
+            type="checkbox"
+            onChange={handleToggle}
+            checked={isChecked}
+          />
+          <span className={styles.slider} />
+        </label>
+      </div>
+      <div className={styles.switchText}>Switch Mode!</div>
+    </div>
+  );
+};
+
+export default Layout;
